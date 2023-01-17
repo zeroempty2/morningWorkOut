7ad2ad6265f66b6123593c58866385e8ea56f965
Seowoo <5w31892p@gmail.com>
2023-01-17 15:00:51 +0900

    fix : Profile entity method add

--- src/main/java/com/sparta/morningworkout/entity/Profile.java
+++ src/main/java/com/sparta/morningworkout/entity/Profile.java
@@ -1,6 +1,17 @@
 package com.sparta.morningworkout.entity;
 
-import jakarta.persistence.*;
+import com.sparta.morningworkout.dto.UpdateCustomerProfileRequest;
+import com.sparta.morningworkout.dto.UpdateSellerProfileRequest;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +31,24 @@ public class Profile {
     @Column
     @Enumerated(EnumType.STRING)
     private CategoryEnum category;
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public Profile(String nickname, String infoContent, CategoryEnum category, User user) {
+        this.nickname = nickname;
+        this.infoContent = infoContent;
+        this.category = category;
+        this.user = user;
+    }
+
+    public void customerUpdate(UpdateCustomerProfileRequest request) {
+        this.nickname = request.getNickname();
+    }
+
+    public void sellerUpdate(UpdateSellerProfileRequest request) {
+        this.nickname = request.getNickname();
+        this.infoContent = request.getInfoContent();
+        this.category = request.getCategory();
+    }
 }
