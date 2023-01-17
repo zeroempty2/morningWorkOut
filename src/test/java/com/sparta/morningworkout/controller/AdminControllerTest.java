f774a0f3519abbedd3238665c4d36497c514ddd6
leeyoungbin <2zeroempty@gmail.com>
2023-01-17 14:16:42 +0900

    <merge> 1월 17일 1차 병합 (영빈)

---
+++ src/test/java/com/sparta/morningworkout/controller/AdminControllerTest.java
@@ -0,0 +1,18 @@
+package com.sparta.morningworkout.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+//@AutoConfigureMockMvc // -> webAppContextSetup(webApplicationContext)
+//@AutoConfigureRestDocs // -> apply(documentationConfiguration(restDocumentation))
+//@ExtendWith(RestDocumentationExtension.class)
+//@WebMvcTest(controllers = UserController.class)
+//@MockBean(JpaMetamodelMappingContext.class)// When using JUnit5
+class AdminControllerTest {
+
+}
