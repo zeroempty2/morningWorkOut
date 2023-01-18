2c45772cb48b2944c11f32787467b99555ccf8de
Seowoo <5w31892p@gmail.com>
2023-01-18 17:22:39 +0900

    <fix> profile function fix

--- src/main/java/com/sparta/morningworkout/dto/profile/ShowCustomerProfileResponseDto.java
+++ src/main/java/com/sparta/morningworkout/dto/profile/ShowCustomerProfileResponseDto.java
@@ -1,18 +1,16 @@
 package com.sparta.morningworkout.dto.profile;
 
 import com.sparta.morningworkout.entity.Profile;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ShowCustomerProfileResponseDto {
-    private long id;
     private String nickname;
 //    private String image;
-    public ShowCustomerProfileResponseDto(long id, Profile profile) {
-        this.id = id;
+    public ShowCustomerProfileResponseDto(Profile profile) {
         this.nickname = profile.getNickname();
     }
-
 }
--- src/main/java/com/sparta/morningworkout/dto/profile/ShowSellerProfileResponseDto.java
+++ src/main/java/com/sparta/morningworkout/dto/profile/ShowSellerProfileResponseDto.java
@@ -2,7 +2,6 @@ package com.sparta.morningworkout.dto.profile;
 
 import com.sparta.morningworkout.entity.CategoryEnum;
 import com.sparta.morningworkout.entity.Profile;
-import com.sparta.morningworkout.entity.SellerRegist;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +9,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ShowSellerProfileResponseDto {
-	private long id;
     private String nickname;
 //    private String image;
     private String infoContent;
     private CategoryEnum category;
 
-	public ShowSellerProfileResponseDto(long id, Profile profiles, SellerRegist sellerRegist) {
-		this.id = id;
+	public ShowSellerProfileResponseDto(Profile profiles) {
 		this.nickname = profiles.getNickname();
-		this.infoContent = sellerRegist.getInfoContent();
-		this.category = sellerRegist.getCategory();
-	}{
+		this.infoContent = profiles.getInfoContent();
+		this.category = profiles.getCategory();
 	}
 }
