8650b101d2423cf68d134e7d926eae9bb438e103
leeyoungbin <2zeroempty@gmail.com>
2023-01-24 11:17:54 +0900

    <fix>포인트 기능 오류 수정

--- src/main/java/com/sparta/morningworkout/dto/Point/AdminGivePresentRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.morningworkout.dto.Point;
-
-import lombok.Getter;
-
-@Getter
-public class AdminGivePresentRequest {
-    private String username;
-    private int givePresent;
-}
